8b5f3556a4470b76e719a640ffcf11c5f2daac4d
akkipaul2000 <skypaul2000@gmail.com>
2024-10-19 23:39:08 +0530

    updated cors policy

--- server.js
+++ server.js
@@ -9,15 +9,12 @@ const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-    }).then(() => console.log('MongoDB connected'))
+    useNewUrlParser: true,
+}).then(() => console.log('MongoDB connected'))
     .catch((err) => console.error('MongoDB connection error:', err));
 
 // Middleware
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json()); // Parse incoming JSON requests
 
 // Routes (No session validation)
